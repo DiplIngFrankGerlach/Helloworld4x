bdd88cae864c36a933bd530d60f423dacbee11c3
Frank Gerlach <frankgerlach.tai@gmx.de>
2025-09-24 09:54:41 +0000

    strncpy_s() nun in mapping.cpp

--- ProjektIncludes.h
+++ ProjektIncludes.h
@@ -2,8 +2,6 @@
 
 #ifdef WIN32_X86
 #include "windows.h"
-#else
-   void strncpy_s(char* destination, int destination_size, const char* source, int numberOfCharacters);
 #endif
 
 #include <iostream>
